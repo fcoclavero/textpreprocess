8f478f9910b3302d3eb22f2a3f19cabab4754990
fcoclavero <fcoclavero32@gmail.com>
2019-01-14 11:11:55 -0300

    Added missing docs.

--- compound_cleaners.py
+++ compound_cleaners.py
@@ -13,8 +13,15 @@ from lemmatizer import lemmatize
 from spell_checker import fix_spelling
 
 
-def clean(texto, *correctores):
-    return reduce(lambda part, func: func(part), correctores, texto)
+def clean(text, *cleaners):
+    """
+    Cleans the given text using the provided cleaning functions.
+    :param text: the text to be cleaned
+    :type: string
+    :return: the clean text
+    :type: string
+    """
+    return reduce(lambda part, func: func(part), cleaners, text)
 
 
 def full_clean(text):
